86c5d8bfb3e64898a3506d0af00a86274f101c71
Zaills <zelephraph@gmail.com>
2023-09-19 15:05:36 +0200

    mouse tped

--- src/render/bonus/mouse.c
+++ src/render/bonus/mouse.c
@@ -21,6 +21,12 @@ void	l_hook(void *param)
 	int32_t	x;
 	int32_t	y;
 
+	//x -> W, y -> H
 	mlx_get_mouse_pos(move->render->mlx, &x, &y);
+	//mlx_hide_mouse;
 	printf("x: %d, y: %d\n", x, y);
+	if (x != WIDTH / 2 || y != WIDTH / 2)
+	{
+		mlx_set_mouse_pos(move->render->mlx, WIDTH / 2, HEIGHT / 2);
+	}
 }
